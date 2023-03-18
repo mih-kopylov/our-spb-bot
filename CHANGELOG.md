be75c606393a579d0e029b0d3ea43c47de39f7f8
Mikhail Kopylov <mih.kopylov@yandex.ru>
2023-03-18 11:50:06 +0300

    #22 Do not try sending a message if user is rate limited

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Changed
+
+- Do not try sending a message if user is rate limited 
+
 ## [0.7.1] - 2023-03-12
 
 ### Fixed
--- internal/state/states.go
+++ internal/state/states.go
@@ -1,5 +1,7 @@
 package state
 
+import "time"
+
 type States interface {
 	// GetState Puts a new user into the context. If the user already exists in the context, it's kept,
 	GetState(userId int64) (*UserState, error)
@@ -7,13 +9,14 @@ type States interface {
 }
 
 type UserState struct {
-	UserId                int64    `firestore:"userId"`
-	CurrentCategoryNodeId string   `firestore:"currentCategoryNodeId"`
-	MessageText           string   `firestore:"messageText"`
-	Files                 []string `firestore:"files"`
-	Login                 string   `firestore:"login"`
-	Password              string   `firestore:"password"`
-	Token                 string   `firestore:"token"`
-	MessageHandlerName    string   `firestore:"messageHandlerName"`
-	SentMessagesCount     int      `firestore:"sentMessagesCount"`
+	UserId                int64     `firestore:"userId"`
+	CurrentCategoryNodeId string    `firestore:"currentCategoryNodeId"`
+	MessageText           string    `firestore:"messageText"`
+	Files                 []string  `firestore:"files"`
+	Login                 string    `firestore:"login"`
+	Password              string    `firestore:"password"`
+	Token                 string    `firestore:"token"`
+	RateLimitedUntil      time.Time `firestore:"rateLimitedUntil"`
+	MessageHandlerName    string    `firestore:"messageHandlerName"`
+	SentMessagesCount     int       `firestore:"sentMessagesCount"`
 }
